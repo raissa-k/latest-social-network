379e4e678bf3d024c6a1522b3805e14e4c6ee8a1
Raissa K <raissakuzerdev@gmail.com>
2022-09-21 16:59:02 -0300

    Added modal responsive menu to navbar * Modal style menu will open on small screens * Added styling to login.ejs * Added "back to top" button to footer.ejs * Profile posts now sorted by date * Added optional navbar to header partial * Added custom title option to header partial

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,7 +5,7 @@ const Comment = require('../models/Comment')
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      const posts = await Post.find({ user: req.user.id });
+      const posts = await Post.find({ user: req.user.id }).sort({ createdAt: "desc" }).lean();
       res.render("profile.ejs", { posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
